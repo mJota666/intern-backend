0458232f203b468b7642aacc03a5d650bfa598e8
mJota666 <micode2744@gmail.com>
2025-05-08 00:01:56 +0700

    BE, edit CORS

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
     console.log('Database seeded!');
   }
   app.enableCors({
-    origin: ['http://localhost:5173','http://localhost:5174'],
+    origin: ['http://localhost:5173','http://localhost:5174', 'http://minhnguyen28.workspace.opstech.org:8081', "http://minhnguyen28.workspace.opstech.org:8082"],
     credentials: false,              
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   });
